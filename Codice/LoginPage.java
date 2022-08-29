697c25b8e3d126a8381a5739738f070d9022a203
Pietro Dudine <105285370+Duddo-Invalid@users.noreply.github.com>
2022-08-29 19:32:55 +0200

    Update LoginPage.java
    Versione aggiornata 29/08/2022

--- Codice/LoginPage.java
+++ Codice/LoginPage.java
@@ -31,6 +31,7 @@ public class LoginPage extends JFrame {
 			public void run() {
 				try {
 					LoginPage frame = new LoginPage();
+					frame.setLocationRelativeTo(null);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -44,67 +45,65 @@ public class LoginPage extends JFrame {
 	 */
 	public LoginPage() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 265, 180);
+		setBounds(100, 100, 265, 160);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(245, 245, 245));
+		contentPane.setBackground(new Color(248, 248, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel UserLbl = new JLabel("Username:\r\n");
 		UserLbl.setHorizontalAlignment(SwingConstants.CENTER);
-		UserLbl.setBounds(34, 27, 66, 26);
+		UserLbl.setBounds(35, 11, 66, 26);
 		contentPane.add(UserLbl);
 		
 		JLabel PasswordLbl = new JLabel("Password:");
 		PasswordLbl.setHorizontalAlignment(SwingConstants.CENTER);
-		PasswordLbl.setBounds(34, 64, 66, 26);
+		PasswordLbl.setBounds(35, 48, 66, 26);
 		contentPane.add(PasswordLbl);
 		
 		userText = new JTextField();
-		userText.setBounds(110, 30, 86, 20);
+		userText.setBounds(111, 14, 86, 20);
 		contentPane.add(userText);
 		userText.setColumns(10);
 		
 		pswField = new JPasswordField();
-		pswField.setBounds(110, 67, 86, 20);
+		pswField.setBounds(111, 51, 86, 20);
 		contentPane.add(pswField);
 		
 		JButton EnterButton = new JButton("Enter");
-		EnterButton.setBackground(new Color(192, 192, 192));
-		EnterButton.setBounds(75, 101, 89, 23);
-		contentPane.add(EnterButton);
-		String userValue = userText.getText();        
-	    String passValue = pswField.getText();  
+		EnterButton.setBackground(new Color(245, 245, 245));
+		EnterButton.setBounds(76, 85, 89, 23);
+		contentPane.add(EnterButton);  
 		EnterButton.addKeyListener(new KeyListener() {
+			
+			@Override
+		    public void keyTyped(KeyEvent e) {}
 
 		    @Override
-		    public void keyTyped(KeyEvent e) {
-
-		    }
-
-		    @Override
-		    public void keyReleased(KeyEvent e) {
-
-		    }
+		    public void keyReleased(KeyEvent e) {}
 
 		    @Override
 		    public void keyPressed(KeyEvent e) {
 		        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+		        	String userValue = userText.getText();        
+		    	    String passValue = pswField.getText(); 
 		        	if (userValue.equals("user") && passValue.equals("1234")) {
 				    	  Home home = new Home();
 				    	  setVisible(false);
 				    	  dispose();
+				    	  home.setLocationRelativeTo(null);
 				    	  home.setAlwaysOnTop(rootPaneCheckingEnabled);
 				    	  home.setVisible(true);
 				    	  
 				    	  
 				      }  
 				      else{  
-				    	  LoginPage tmp = new LoginPage();
+				    	  JFrame tmp = new JFrame();
 				    	  userText.setText("");
 				    	  pswField.setText("");
 				    	  tmp.setAlwaysOnTop(true);
+				    	  tmp.setLocationRelativeTo(null);
 				    	  JOptionPane.showMessageDialog(tmp, "User o Password errati!!");
 				      }
 				 }
@@ -113,25 +112,27 @@ public class LoginPage extends JFrame {
 		});
 		
 		EnterButton.addActionListener(new ActionListener() {
-			 public void actionPerformed(ActionEvent e){     
-			         
-			      if (userValue.equals("user") && passValue.equals("1234")) {
-			    	  Home home = new Home();
-			    	  setVisible(false);
-			    	  dispose();
-			    	  home.setAlwaysOnTop(rootPaneCheckingEnabled);
-			    	  home.setVisible(true);
-			    	  
-			    	  
-			      }  
-			      else{  
-			    	  LoginPage tmp = new LoginPage();
-			    	  userText.setText("");
-			    	  pswField.setText("");
-			    	  tmp.setAlwaysOnTop(true);
-			    	  JOptionPane.showMessageDialog(tmp, "User o Password errati!!");
-			      }
-			 }
+			 public void actionPerformed(ActionEvent e){
+				 
+				 String userValue = userText.getText();        
+				 String passValue = pswField.getText();
+			     if (userValue.equals("user") && passValue.equals("1234")) {
+			    	 Home home = new Home();
+			    	 setVisible(false);
+			    	 dispose();
+			    	 home.setAlwaysOnTop(rootPaneCheckingEnabled);
+			    	 home.setLocationRelativeTo(null);
+			    	 home.setVisible(true); 
+			    }  
+			    else{  
+			    	JFrame tmp = new JFrame();
+			    	userText.setText("");
+			    	pswField.setText("");
+			    	tmp.setAlwaysOnTop(true);
+			    	tmp.setLocationRelativeTo(null);
+			    	JOptionPane.showMessageDialog(tmp, "User o Password errati!!");
+			   }
+			}
 		});
 		
 	}
