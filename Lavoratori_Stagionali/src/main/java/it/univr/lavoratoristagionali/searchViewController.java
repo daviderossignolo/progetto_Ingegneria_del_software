2bf924ab9b54817ddd73ab34c59f90ecd848f312
Davide Rossignolo <55827662+itsRoss@users.noreply.github.com>
2022-09-27 00:52:21 +0200

    Delete Lavoratori_Stagionali directory

--- Lavoratori_Stagionali/src/main/java/it/univr/lavoratoristagionali/searchViewController.java
+++
@@ -1,4 +0,0 @@
-package it.univr.lavoratoristagionali;
-
-public class searchViewController {
-}
--- Lavoratori_Stagionali/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module it.univr.lavoratoristagionali {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens it.univr.lavoratoristagionali to javafx.fxml;
-    exports it.univr.lavoratoristagionali;
-}
